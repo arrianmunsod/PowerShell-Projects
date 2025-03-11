1b785283f47f82ff549f77a6aa623cac158f3d6d
Arrian Munsod <arrianmunsod657@gmail.com>
2025-03-11 08:21:59 +0800

    Random Commit

--- Creating-An-SMB-File-Share.ps1
+++ Creating-An-SMB-File-Share.ps1
@@ -1,3 +1,22 @@
+$FeatureName = "File-Services"
+$ComputerName = "server1"
+
+# Check if the feature is already installed
+if (-not (Get-WindowsFeature -Name $FeatureName -ComputerName $ComputerName | Where-Object {$_.Installed -eq $true})) {
+    # Feature is not installed, install it
+    Write-Host "Feature '$FeatureName' is not installed on '$ComputerName'. Installing..."
+    Install-WindowsFeature -Name $FeatureName -ComputerName $ComputerName -Verbose
+    if ($LASTEXITCODE -eq 0) {
+        Write-Host "Feature '$FeatureName' installed successfully on '$ComputerName'."
+    } else {
+        Write-Warning "Feature '$FeatureName' installation failed on '$ComputerName'."
+    }
+
+} else {
+    # Feature is already installed, skip
+    Write-Host "Feature '$FeatureName' is already installed on '$ComputerName'. Skipping installation."
+}
+
 # You can type this directly to the PowerShell terminal 
 New-Item -ItemType Directory -Path "C:\Test-Folder-01"
 New-SmbShare -Name "Test-Folder-01" -Path "C:\Test-Folder-01" -CachingMode None
